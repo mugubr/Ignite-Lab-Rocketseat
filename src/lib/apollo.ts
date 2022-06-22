35495638104fa526c583950fcaea2fa46959327a
mugubr <miguimuniz@gmail.com>
2022-06-22 15:12:39 -0300

    Aula 2-estrutura visual da página

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'http://localhost:8080/api/v1/users',
+    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4psz06t2btx01xs14l3gtms/master',
     cache: new InMemoryCache()
 })
 
