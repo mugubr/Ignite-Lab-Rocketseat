02b26f915b5d76159f47495c81b28f7335221fa1
mugubr <miguimuniz@gmail.com>
2022-06-23 13:33:59 -0300

    Aula 3-roteamento e player de vídeo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { gql, useQuery } from "@apollo/client";
-import {Event } from "./pages/Event";
+import { ApolloProvider } from "@apollo/client";
+import { Router } from "./Router";
+import {client} from "./lib/apollo"
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Event />
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+        <Router />
+        </BrowserRouter>
+      </ApolloProvider>
     </>
   );
 }
