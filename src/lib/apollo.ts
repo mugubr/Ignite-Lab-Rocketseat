bddf7f8e6a1ad239cdae158a05d0768a6a882f46
mugubr <miguimuniz@gmail.com>
2022-06-27 14:45:07 -0300

    Aula 3 - inscrição via GraphQL

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4psz06t2btx01xs14l3gtms/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN} `
+    },
     cache: new InMemoryCache()
 })
 
